109848fdfdbeb1fffb84c3407df567a5f54026da
NessB <161356333+nicolasbonaa@users.noreply.github.com>
2025-05-21 12:58:51 -0300

    Created Server

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3333",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+});
